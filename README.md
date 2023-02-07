ee71c73d7e6b30455a8db42c8db965e761cd4da0
Lubna <shaikhls2421@gmail.com>
2023-02-07 20:25:44 +0530

    checking readme

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Cypress is a next generation automation testing tool for Modern Web Apps.
 text cases in Synchronouse fasion, this is on of its features.
 
 
-DAY:
+DAYS:
 >> 0-6    -All basic, its pros and cons , Nature. and making some tests and assertion.
 
->> 7 -
+>> 7 -<cypress automatically handles the alerts>
--- cypress/integration/examples/Test3.js
+++ cypress/integration/examples/Test3.js
@@ -8,6 +8,13 @@ describe('test three- new window and alert handling',()=>{
         cy.log('hi there')
         cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
 
-        
+        cy.get('#alertbtn').click()
+        cy.get('#confirmbtn').click()
+
+        cy.on('window:alert',(str)=>{
+            expect(str).equal('Hello , share this practice page and share your knowledge')
+        })
+
+
     })
 })
