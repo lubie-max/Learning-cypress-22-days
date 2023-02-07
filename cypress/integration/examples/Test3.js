ee6a2beed875e1663f9dc8ca8abec5f50e8ab600
Lubna <shaikhls2421@gmail.com>
2023-02-07 18:37:14 +0530

    day 7:

---
+++ cypress/integration/examples/Test3.js
@@ -0,0 +1,13 @@
+/// <reference types="Cypress" />
+
+
+describe('test three- new window and alert handling',()=>{
+
+    it('alerts and child window ',()=>{
+
+        cy.log('hi there')
+        cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
+
+        
+    })
+})
