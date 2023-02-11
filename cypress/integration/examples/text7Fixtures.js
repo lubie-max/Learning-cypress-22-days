ab9dcb31af19f95e8e6b69a0627941f2a7cff3ef
Lubna <shaikhls2421@gmail.com>
2023-02-11 23:38:50 +0530

    day:11 Fixtures

--- cypress/integration/examples/text7Fixtures.js
+++ cypress/integration/examples/text7Fixtures.js
@@ -1,12 +1,15 @@
 /// <reference types="Cypress" />
 
+
 describe('More Aboute Fixtures',()=>{
 
     it("first test", ()=>{
+        // fixtures are nothing but files which provides Test data for tast cases written in JSON format.
+
 
         cy.visit('https://rahulshettyacademy.com/angularpractice/')
 
-        cy.get('input[name="name"]').eq(1).type('Lubna Shaikh')
+        // cy.get('input[name="name"]').eq(1).type(this.regdata.name)
         cy.get('input[name="email"]').type('ls@mail.com')
         cy.get("input[type='password']").type('12345678')
         cy.get('input[type="checkbox"').check()
