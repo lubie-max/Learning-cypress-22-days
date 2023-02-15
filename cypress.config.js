ec125dd02fde4217168261b887bf9873be062267
Lubna <shaikhls2421@gmail.com>
2023-02-15 20:58:37 +0530

    day15

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,9 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  env:{
+    url:"https://rahulshettyacademy.com/angularpractice/shop/"
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
